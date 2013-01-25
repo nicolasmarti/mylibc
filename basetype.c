60e30daa4e109fff79b4872eb4edc57be569a7b9
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-01-25 14:28:34 +0900

    added more comparison functions

--- basetype.c
+++ basetype.c
@@ -11,3 +11,94 @@ cmp_t uint8_cmp(const uint8_t i1, const uint8_t i2){
   return Gt;
 
 }
+
+
+cmp_t sint8_cmp(const sint8_t i1, const sint8_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
+
+
+cmp_t uint16_cmp(const uint16_t i1, const uint16_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
+
+
+cmp_t sint16_cmp(const sint16_t i1, const sint16_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
+
+
+cmp_t uint32_cmp(const uint32_t i1, const uint32_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
+
+
+cmp_t sint32_cmp(const sint32_t i1, const sint32_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
+
+
+cmp_t uint64_cmp(const uint64_t i1, const uint64_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
+
+
+cmp_t sint64_cmp(const sint64_t i1, const sint64_t i2){
+
+  if (i1 == i2)
+    return Eq;
+
+  if (i1 < i2)
+    return Lt;
+
+  return Gt;
+
+}
--- basetype.h
+++ basetype.h
@@ -51,6 +51,163 @@ typedef enum {Lt, Eq, Gt} cmp_t;
 
 cmp_t uint8_cmp(const uint8_t i1, const uint8_t i2);
 
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t sint8_cmp(const sint8_t i1, const sint8_t i2);
+
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t uint16_cmp(const uint16_t i1, const uint16_t i2);
+
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t sint16_cmp(const sint16_t i1, const sint16_t i2);
+
+
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t uint32_cmp(const uint32_t i1, const uint32_t i2);
+
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t sint32_cmp(const sint32_t i1, const sint32_t i2);
+
+
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t uint64_cmp(const uint64_t i1, const uint64_t i2);
+
+/*@
+  assigns \nothing;
+  
+  behavior lt:
+    assumes i1 < i2;
+    ensures \result == Lt;
+
+  behavior eq:
+    assumes i1 == i2;
+    ensures \result == Eq;
+
+  behavior gt:
+    assumes i1 > i2;
+    ensures \result == Gt;
+
+  complete behaviors;
+  disjoint behaviors;
+
+*/
+
+cmp_t sint64_cmp(const sint64_t i1, const sint64_t i2);
+
+
 ////////////////////////////////////////////////
 // bool
 
