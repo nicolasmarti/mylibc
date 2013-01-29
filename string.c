f8c35551237d708d23eb6a4bd964bae24207726e
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-01-29 12:05:18 +0900

    reput the code of strcmp

--- string.c
+++ string.c
@@ -17,4 +17,38 @@ size_t my_strlen(const string_t s){
 
 }
 
+cmp_t my_strcmp(const string_t s1, const string_t s2){
+
+  index_t counter = 0;
+
+  /*@
+    loop invariant 
+       \forall size_t sz1; string_size(s1, sz1) ==>
+       \forall size_t sz2; string_size(s2, sz2) ==>
+       counter <= sz1 && counter <= sz2 &&
+       (\forall index_t i; 0 <= i < counter ==> s1[i] == s2[i]);
+    loop assigns counter;
+   */
+  for (counter = 0; ; ++counter)
+    {
+
+      switch (uint8_cmp(s1[counter], s2[counter])){
+
+      case Eq:
+	if (s1[counter] == 0 && s2[counter] == 0)
+	  return Eq;
+	break;
+
+      case Lt:
+	return Lt;
+	
+      case Gt:
+	return Gt;
+
+      }
+
+      
+    }
+
+}
 
