70ea86e2f58a4ba2fc43f048ef61f1070be9e71c
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-01-29 16:51:18 +0900

    remove craps

--- test.c
+++ test.c
@@ -1,15 +1,10 @@
 #include "basetype.h"
 #include "string.h"
 
-#include <stdio.h>
-
 // just a test for the lib
 
 int main(int argc, char** argv, char** arge)
 {
-
-  // not handle by the default memory model ...
-  // printf("%zd\n", my_strlen((const string_t)"doudou"));
 
   return 0;
 }
