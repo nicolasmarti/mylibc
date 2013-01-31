bb16316e2612c6dd48054cf548b5b98e72e06db6
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-01-31 10:33:58 +0900

    added push function for round stack

--- round_stack.c
+++ round_stack.c
@@ -1,13 +1,43 @@
 #include "round_stack.h"
 #include <stdlib.h>
+#include <string.h>
 
 uint8_t init_round_stack(round_stack_t *s){
 
-  if (0 == (s->storage = (char*)(malloc(s->element_size * s->nb_storable_element))))
+  // try to allocate, and if it fails return failed
+  if (0 == (s->storage = (any_ptr_t)(malloc(s->element_size * s->nb_storable_element))))
     return 0;
 
-  s->start_slot = s->stop_slot = s->nb_storable_element = 0;
+  // else just set the structures fields and return succeed
+  s->start_slot = s->current_slot = s->nb_storable_element = 0;
   
   return 1;
+
+}
+
+uint8_t round_stack_push(round_stack_t* s, any_ptr_t elt){
+
+  // looking if the stack is full or not
+  if (s->start_slot != s->current_slot || s->nb_stored_element == 0){
+    // not full, do the insertion
+    
+    // getting the place were to put the element
+    any_ptr_t storage = s->storage + s->element_size * s->current_slot;
+    
+    // and copying it
+    memcpy(storage, elt, s->element_size);
+    
+    // updates stack fields: current_slot and nb_stored_element
+    s->current_slot = (s->current_slot + 1) % s->nb_storable_element;
+    s->nb_stored_element++;
+    
+    // return succeed
+    return 1;
+
+  } else {
+    // full: return failed
+    return 0;
+
+  }
 
 }
--- round_stack.h
+++ round_stack.h
@@ -17,10 +17,10 @@ typedef struct {
   size_t nb_stored_element;
 
   // start,end slots
-  index_t start_slot, stop_slot;
+  index_t start_slot, current_slot;
 
   // the storage
-  char* storage;
+  any_ptr_t storage;
 
 } round_stack_t;
 
@@ -29,41 +29,88 @@ typedef struct {
   @predicate wf_round_stack(round_stack_t st) = 
   @  // validity of the storage  
   @  \valid(st.storage+(0..st.element_size*st.nb_storable_element)) &&
-  @  // validity ranges for start / stop pointer
+  @  // validity ranges for start / current pointer
   @  0 <= st.start_slot < st.nb_storable_element &&  
-  @  0 <= st.stop_slot < st.nb_storable_element &&
-  @  // link between start/stop slots pointers and nb_stored_elements
-  @  (st.start_slot == st.stop_slot ==> (st.nb_stored_element == st.nb_storable_element || st.nb_stored_element == 0)) &&  
-  @  (st.start_slot < st.stop_slot ==> st.nb_stored_element == st.stop_slot - st.start_slot) &&
-  @  (st.start_slot > st.stop_slot ==> st.nb_stored_element == st.nb_storable_element - (st.start_slot - st.stop_slot));     
+  @  0 <= st.current_slot < st.nb_storable_element &&
+  @  // link between start/current slots pointers and nb_stored_elements
+  @  (st.start_slot == st.current_slot ==> (st.nb_stored_element == st.nb_storable_element || st.nb_stored_element == 0)) &&  
+  @  (st.start_slot < st.current_slot ==> st.nb_stored_element == st.current_slot - st.start_slot) &&
+  @  (st.start_slot > st.current_slot ==> st.nb_stored_element == st.nb_storable_element - (st.start_slot - st.current_slot));     
  */
 
 // not yet handled ... 
 /*@ type invariant round_stack_invariant(round_stack_t s) = wf_round_stack(s) ; */ 
 
+////////////////////////////////////////////////////////////////////////////////////
 // initialize function
-// it takes a pointer to a round_stack header, build the storage and sets the nb_stored_element, start_slot, stop_slot and storage fields
+
+// it takes a pointer to a round_stack header, build the storage and sets the nb_stored_element, start_slot, current_slot and storage fields
 // returns 0 if failed, and 1 if succeed
 
 /*@
   @requires \valid(\base_addr(s)+(0..\block_length(s)-1));
   @requires \valid(s);
   @//@requires s->element_size*s->nb_storable_element < \offset_max(s->storage);
+  @allocates(s->storage);
   @behavior failed:
   @   assigns s->storage;
   @   ensures \result == 0;
   @   ensures s->storage == \null;
   @behavior succeed:
-  @  assigns s->nb_stored_element, s->start_slot, s->stop_slot, s->storage;
+  @  assigns s->nb_stored_element, s->start_slot, s->current_slot, s->storage;
   @  ensures \result == 1;
   @  ensures s->start_slot == 0;
-  @  ensures s->stop_slot == 0;
+  @  ensures s->current_slot == 0;
   @  ensures s->nb_stored_element == 0;
   @  ensures wf_round_stack(*s);
   @  ensures \separated(s, s->storage);
-  @  allocates(s->storage);
+  @  
+  @  complete behaviors;
+  @  disjoint behaviors;
  */
 
 uint8_t init_round_stack(round_stack_t *s);
+
+////////////////////////////////////////////////////////////////////////////////////
+// pushing on the stack
+
+// returns 0 if failed, and 1 if succeed
+
+/*@ // definition of a full roundstack
+  @ predicate round_stack_full(round_stack_t s) =
+  @   s.start_slot == s.current_slot && s.nb_stored_element == s.nb_storable_element;
+  @
+  @// definition of an empty roundstack
+  @ predicate round_stack_empty(round_stack_t s) =
+  @   s.start_slot == s.current_slot && s.nb_stored_element == 0;
+  @*/
+
+
+/*@
+  @requires \valid(\base_addr(s)+(0..\block_length(s)-1));
+  @requires \valid(s);
+  @requires \valid(elt+(0..s->element_size-1));
+  @requires wf_round_stack(*s);
+  @
+  @ensures wf_round_stack(*s);
+  @
+  @behavior failed:
+  @  assumes round_stack_full(*s);
+  @  assigns \nothing;
+  @  ensures \result == 0;
+  @  
+  @behavior succeed:
+  @  assumes !round_stack_full(*s);
+  @  assigns s->nb_stored_element, s->current_slot, s->storage[(s->current_slot*s->element_size)..((s->current_slot+1)*s->element_size - 1)];
+  @  ensures s->storage[(s->current_slot*s->element_size)..((s->current_slot+1)*s->element_size - 1)] == elt[0..s->element_size-1];
+  @  ensures s->nb_stored_element == \old(s->nb_stored_element) + 1;
+  @  ensures \result == 1;
+  @  
+  @
+  @complete behaviors;
+  @disjoint behaviors;
+  @*/
+
+uint8_t round_stack_push(round_stack_t* s, any_ptr_t elt);
 
 #endif
