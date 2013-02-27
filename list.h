2cf0e62b8bbc93e94b52fb5b98ecc0fc7cb0ccf7
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-02-27 20:10:39 +0900

    new patch works ... but provers are still dumb

--- list.h
+++ list.h
@@ -11,10 +11,18 @@ typedef struct _list {
   @ }
   @ 
   @ axiomatic wf_list {
-  @ axiom empty{L}: \forall list_t* l; l == \null ==> wf_list(l);
-  @ axiom non_empty{L}: \forall list_t* l; l != \null ==> \valid(l) ==> wf_list(l->next) ==> wf_list(l);
-  @ axiom wf_list_inversion{L}: \forall list_t* l; wf_list(l) ==>
+  @ axiom wf_list_inversion: \forall list_t* l; wf_list(l) ==>
   @ l == \null || (l != \null && \valid(l) && wf_list(l->next));
   @ }
+  @
+*/
+
+/*@
+  @ inductive reachable{L}(list_t* l1, list_t* l2, integer n) {
+  @ case empty: \forall list_t* l; reachable(l, l, 0);
+  @ case non_empty: \forall list_t *l1, *l2; \forall integer n; l1 != \null ==> \valid(l1) ==> reachable(l1->next, l2, n) ==> reachable(l1, l2, n + 1);
+  @ }
+  @ 
+  @ lemma reachable_geq0: \forall list_t *l1, *l2; \forall integer n; reachable(l1, l2, n) ==> n >= 0;
   @
 */
