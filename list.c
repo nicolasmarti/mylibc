db1afee702187fe777f55247834610c68190ff0b
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-03-01 21:58:26 +0900

    new list predicates

--- list.c
+++ list.c
@@ -1,1 +1,136 @@
 #include "list.h"
+
+/*@ predicate is_circular_singly_list_loop_assert{L}(
+  @ singly_list_t *lst, 
+  @ singly_list_t *fast, 
+  @ singly_list_t *slow, 
+  @ int dist_lst_slow,
+  @ int dist_slow_fast,
+  @ integer dist) =
+  @   fast != slow &&
+  @   fast != \null &&
+  @   slow != \null &&
+  @   wf_singly_list(slow) &&
+  @   wf_singly_list(fast) &&
+  @   wf_singly_list(lst) &&
+  @   \valid(fast) &&
+  @   \valid(slow) &&
+  @   reachable(lst, slow, dist_lst_slow) &&
+  @   reachable(slow, fast, dist_slow_fast) &&
+  @   reachable(lst, fast, dist_lst_slow + dist_slow_fast) &&
+  @   dist_slow_fast > dist &&
+  @   dist_lst_slow >= 0;      
+*/
+
+
+int is_circular_singly_list(singly_list_t* lst){
+
+  singly_list_t *slow, *fast;
+  unsigned int cpter;
+
+
+  if (lst == 0) {
+    //@ assert !circular_singly_list(lst);
+    return 0;
+  }
+
+  //@ assert lst != \null;
+
+  if (lst == lst->next){
+    //@ assert reachable(lst, lst, 0) && reachable (lst, lst, 1);
+    //@ assert circular_singly_list(lst);
+    return 1;
+  }
+
+  if (lst->next == 0){
+    //@ assert !circular_singly_list(lst);
+    return 0;
+  }
+
+  // seems to be a known bug: http://bts.frama-c.com/view.php?id=387
+  // this is a ticket from (2010-02-01 09:46) still not solved ...
+  // a related message: http://stackoverflow.com/questions/9956173/acsl-set-logic-frama-c-syntax-error
+  // and finally the frama-c-acsl-implementation manuel says that it is not supported
+  // http://frama-c.com/download/frama-c-acsl-implementation.pdf
+  // it will make the verification to fail on rte assert on both ghost variable
+  //@ ghost int dist_slow_fast = 1;
+  //@ ghost int dist_lst_slow = 0;
+
+  slow = lst;
+  fast = lst->next;
+
+  /////////////////////////////////////////////////////////////////////////////////
+
+  /*@ loop invariant is_circular_singly_list_loop_assert(lst, fast, slow, dist_lst_slow, dist_slow_fast, 0);
+    @ loop assigns fast, slow, dist_slow_fast, dist_lst_slow;
+  */
+  while (1) {
+
+    //@ assert is_circular_singly_list_loop_assert(lst, fast, slow, dist_lst_slow, dist_slow_fast, 0);
+
+    // advance fast by one
+    if (fast-> next == 0){
+      //@ assert !circular_singly_list(lst);
+      return 0;
+    } else {
+      fast = fast-> next;
+      //@ ghost ++dist_slow_fast;
+    }
+
+    if (fast == slow){
+      //@ assert circular_singly_list(lst);
+      return 1;
+    }
+    
+    //@ assert is_circular_singly_list_loop_assert(lst, fast, slow, dist_lst_slow, dist_slow_fast, 1);
+
+    // advance fast by one
+    if (fast-> next == 0){
+      //@ assert !circular_singly_list(lst);
+      return 0;
+    } else {
+      fast = fast-> next;
+      //@ ghost ++dist_slow_fast;
+    }
+
+    if (fast == slow){
+      //@ assert circular_singly_list(lst);
+      return 1;
+    }
+
+    //@ assert is_circular_singly_list_loop_assert(lst, fast, slow, dist_lst_slow, dist_slow_fast, 2);
+
+    // advance fast by one
+    if (fast-> next == 0){
+      //@ assert !circular_singly_list(lst);
+      return 0;
+    } else {
+      fast = fast-> next;
+      //@ ghost ++dist_slow_fast;
+    }
+
+    if (fast == slow){
+      //@ assert circular_singly_list(lst);
+      return 1;
+    }
+
+    //@ assert is_circular_singly_list_loop_assert(lst, fast, slow, dist_lst_slow, dist_slow_fast, 3);
+
+    // advance slow by one
+    slow = slow->next;
+    //@ ghost ++dist_lst_slow;
+    //@ ghost --dist_slow_fast;
+
+    if (fast == slow){
+      //@ assert circular_singly_list(lst);
+      return 1;
+    }
+
+    //@ assert is_circular_singly_list_loop_assert(lst, fast, slow, dist_lst_slow, dist_slow_fast, 2);
+
+  }
+  /////////////////////////////////////////////////////////////////////////////////
+  
+
+}
+
--- list.h
+++ list.h
@@ -1,28 +1,92 @@
-
-typedef struct _list {
-  double elt;
-  struct _list* next;
-} list_t;
+typedef struct _singly_list {
+  int elt;
+  struct _singly_list* next;
+} singly_list_t;
 
 /*@
-  @ inductive wf_list{L}(list_t* l) {
-  @ case empty: \forall list_t* l; l == \null ==> wf_list(l);
-  @ case non_empty: \forall list_t* l; l != \null ==> \valid(l) ==> wf_list(l->next) ==> wf_list(l);
-  @ }
+  @ type list<A> = Nul | Cons(A, list<A>);
   @ 
-  @ axiomatic wf_list {
-  @ axiom wf_list_inversion: \forall list_t* l; wf_list(l) ==>
-  @ l == \null || (l != \null && \valid(l) && wf_list(l->next));
-  @ }
-  @
+  @ 
 */
 
 /*@
-  @ inductive reachable{L}(list_t* l1, list_t* l2, integer n) {
-  @ case empty: \forall list_t* l; reachable(l, l, 0);
-  @ case non_empty: \forall list_t *l1, *l2; \forall integer n; l1 != \null ==> \valid(l1) ==> reachable(l1->next, l2, n) ==> reachable(l1, l2, n + 1);
+  @ inductive wf_singly_list{L}(singly_list_t* lst){
+  @   case empty: \forall singly_list_t* lst; lst == \null ==> wf_singly_list(lst);
+  @   case non_empty: \forall singly_list_t* lst; lst != \null ==> \valid(lst) ==> wf_singly_list(lst->next) ==> wf_singly_list(lst);
+  @   }
+  @   
+  @ axiomatic wf_singly_list {   
+  @ axiom wf_singly_list_inversion: \forall singly_list_t* lst; wf_singly_list(lst) ==> 
+  @  lst == \null ||
+  @  (lst != \null && \valid(lst) && wf_singly_list(lst->next));
   @ }
   @ 
-  @ lemma reachable_geq0: \forall list_t *l1, *l2; \forall integer n; reachable(l1, l2, n) ==> n >= 0;
+  @ 
+*/
+
+/*@
+  @ inductive singly_list_length{L}(singly_list_t* lst, integer sz){
+  @   case empty: \forall singly_list_t* lst; lst == \null ==> singly_list_length(lst, 0);
+  @   case non_empty: \forall singly_list_t* lst; \forall integer sz; lst != \null ==> \valid(lst) ==> singly_list_length(lst->next, sz) ==> singly_list_length(lst, sz+1);
+  @   }
+  @   
+  @ axiomatic singly_list_length {   
+  @  axiom singly_list_length_inversion: \forall singly_list_t* lst; \forall integer sz; singly_list_length(lst, sz) ==> 
+  @  (lst == \null && sz == 0) ||
+  @  (\exists integer sz2; lst != \null && \valid(lst) && singly_list_length(lst->next, sz2) && sz == sz2+1);
+  @ }
+  @ 
+*/
+
+
+/*@
+  @ inductive reachable{L}(singly_list_t *lst1, singly_list_t *lst2, integer n){
+  @ case reachable1: \forall singly_list_t *lst; reachable(lst, lst, 0);
+  @ case reachable2: \forall singly_list_t *lst1, *lst2; \forall integer n1, n2; lst1 != \null ==> \valid(lst1) ==> n2 == n1 + 1 ==> reachable(lst1->next, lst2, n1) ==> reachable(lst1, lst2, n2);
+  @ }
+  @ 
+  @  
+  @ 
+*/ 
+
+/*@  
+  @ axiomatic reachable {
+  @  axiom reachable_inversion: \forall singly_list_t *lst1, *lst2; \forall integer n; reachable(lst1, lst2, n) ==>
+  @  (lst1 == lst2 && n == 0) || (n > 0 && lst1 != \null && \valid(lst1) && reachable(lst1->next, lst2, n - 1));
+  @  axiom reachable_trans: \forall singly_list_t *lst1, *lst2, *lst3; \forall integer n1, n2, n3; n3 == n1 + n2 ==> reachable(lst1, lst2, n1) ==> reachable(lst2, lst3, n2) ==> reachable(lst1, lst3, n3);
+  @  // fora now as axioms ...
+  @  axiom reachable_lhs_next: \forall singly_list_t *lst; lst != \null && \valid(lst) ==> reachable(lst, lst->next, 1);
+  @  axiom reachable_rhs_next: \forall singly_list_t *lst1, *lst2; \forall integer n; reachable(lst1, lst2, n) && lst2 != \null && \valid(lst2) ==> reachable(lst1, lst2->next, n + 1);
+  @ }
+  @ 
+  @ 
+  @ 
+  @ predicate circular_singly_list{L}(singly_list_t *lst) = \exists singly_list_t* lst2; \exists integer n; 
+  @    reachable(lst, lst2, n) && (\exists integer m; m >0 && reachable (lst2, lst2, m));
+  @    
+  @ axiomatic circular_singly_list {
+  @ // this should be a lemma ...
+  @ axiom null_reachable_non_circular: \forall singly_list_t *lst; \forall integer n; reachable(lst, \null, n) ==> !circular_singly_list(lst);
+  @ }
+  @ 
   @
 */
+
+/*@ // algorithm for detecting circular list
+  @ requires wf_singly_list(lst);
+  @ ensures wf_singly_list(lst);
+  @ assigns \nothing;
+  @ behavior non_circular:
+  @   assumes !circular_singly_list(lst);
+  @   ensures \result == 0;
+  @   
+  @ behavior circular:
+  @   assumes circular_singly_list(lst);
+  @   ensures \result == 1;
+  @   
+  @ complete behaviors;
+  @ disjoint behaviors;
+  @ 
+*/
+
+int is_circular_singly_list(singly_list_t* lst);
