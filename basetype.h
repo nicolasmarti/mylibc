95e131ecbe9af60a9cf2cf8d9ced0fec0f9d3936
Nicolas Marti <nicolas.marti.japon@gmail.com>
2014-08-25 21:15:38 +0900

    remove a redefinition

--- basetype.h
+++ basetype.h
@@ -25,7 +25,7 @@ typedef unsigned long uint_t;
 // the types used for size and to index into an array
 
 // here we make the arbitrary choice of uint32_t, should be sufficient
-typedef uint_t size_t;
+//typedef uint_t size_t;
 
 // definition/lemma about the minimum of size_t
 /*
