361f25fa8958fabd3adc0e8b0ca6e5edc109e623
Niclas Marti <nicolas.marti.japon@gmail.com>
2016-06-08 21:50:05 +0900

    fixed sum_max

--- verifythis.c
+++ verifythis.c
@@ -1,6 +1,15 @@
 
-//@ logic integer array_int_sum{L}(int *array, integer n) = (n < 0) ? 0 : (array[n] + array_int_sum(array, n-1));
-
+/*@ logic integer array_int_sum{L}(int *array, integer n) = (n < 0) ? 0 : (array[n] + array_int_sum(array, n-1));
+  @
+  @ axiomatic array_int_sum_extra {
+  @
+  @ axiom array_int_sum_invariant{L1, L2}:
+  @  \forall int* a, integer n;
+  @  (\forall integer k; 0 <= k < n ==> \at(a[k], L1) == \at(a[k], L2)) ==>
+  @  array_int_sum{L1}(a, n) == array_int_sum{L2}(a, n);
+  @
+  @ }
+*/
 
 /*@ requires \valid(a+(0..(N-1)));
   @ requires N > 0;
@@ -11,16 +20,20 @@
   @ assigns *sum, *max;
   @
   @ ensures \forall integer k; 0 <= k < N ==> a[k] <= *max;
+  @ ensures \exists integer k; 0 <= k < N && a[k] == *max;
+  @ ensures *sum == array_int_sum(a, N-1);
 */
 
 
-void sum_max2(int* a, int N, int* max, int* sum){
+void sum_max(int* a, int N, int* max, int* sum){
 
-  *sum = 0;
-  *max = 0;
-  int i = 0;
+  int i = 1;
+
+  *sum = a[0];
+  *max = a[0];
   
   /*@ loop invariant \forall integer k; 0 <= k < i ==> a[k] <= *max;
+    @ loop invariant \exists integer k; 0 <= k < i && a[k] == *max;
     @ loop invariant *sum == array_int_sum(a, i-1);
     @ loop invariant 0 <= i <= N;
     @ loop invariant \separated(max, sum, a+(0..(N-1)));
@@ -28,31 +41,13 @@ void sum_max2(int* a, int N, int* max, int* sum){
     @ loop variant (N - i);
   */
   for (; i<N; i++){
-
-    //@ assert i < N;
-
-    //@ assert *sum == array_int_sum(a, i-1);
     
     if (*max < a[i]){
       
       *max = a[i];
-
-      //@ assert *sum == array_int_sum(a, i-1);
       
-      
-    } else {
-
-
-      //@ assert *sum == array_int_sum(a, i-1);
-
-
-    }
-
-    //@ assert *sum == array_int_sum(a, i-1);
-    
+    }    
     *sum += a[i];
-
-    //@ assert *sum == array_int_sum(a, i-1) + a[i];
     
   }
 
